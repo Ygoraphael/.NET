9fd686c885aad01ae448be1a4fd9b78eb080cb71
Ygor raphael <ygoraphael@gmail.com>
2021-01-25 17:48:39 +0000

    Exemplo algoritimo pilha Console terminal

---
+++ Pilha/Pilha/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pilha
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Pilha = new Queue();
+            
+            Pilha.StackUp(1);
+            Pilha.StackUp(10);
+            Pilha.StackUp(100);
+
+            Console.WriteLine(Pilha.UnStack());
+            Console.WriteLine(Pilha.UnStack());
+            Console.WriteLine(Pilha.UnStack());
+        }
+    }
+}
